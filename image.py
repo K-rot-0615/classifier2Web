2a76666de12319ef41b38079967efae92a2e2430
K-rot-0615 <dk.mat.18.of.a.challenge@gmail.com>
2016-10-24 02:33:29 +0900

    save
    save face-image with fixed size

--- image.py
+++ image.py
@@ -1,14 +1,10 @@
 # -*- coding:utf-8 -*-
 
 import argparse
-import numpy as np
 import cv2
 import time
-import threading
 import os
 import os.path
-import re
-from datetime import datetime
 
 camera = 1
 cameraNum = [0, 1, 2, 3]
@@ -16,14 +12,12 @@ x = []
 y = []
 width = []
 height = []
+global count
 
 
-def faceDetect(frame, cameraNum, save_file):
+def faceDetect(frame, cameraNum, save_file, resize):
 
 	cascade_path = "./lib/haarcascade_frontalface_default.xml"
-
-	global timeCount
-	global count
 	frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	cascade = cv2.CascadeClassifier(cascade_path)
 	facerect = cascade.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=3, minSize=(10, 10))
@@ -32,10 +26,9 @@ def faceDetect(frame, cameraNum, save_file):
 	if len(facerect) > 0:
 		print 'Face has been detected'
 		color = (255)
-		now = datetime.now().strftime('%Y%m%d%H%M%S')
 		image_path = "./datasets/" + save_file
-		if os.path.isdir(imagePath) == False:
-			os.mkdir(imagePath)
+		if os.path.isdir(image_path) == False:
+			os.mkdir(image_path)
 			print 'make image path'
 		else:
 			print 'path already exists'
@@ -45,15 +38,15 @@ def faceDetect(frame, cameraNum, save_file):
 	i = 0
 	for rect in facerect:
 		print rect[0]
-		timeCount += 1
 		x.append(rect[0] - 50)
 		y.append(rect[1] - 50)
 		width.append(rect[2] + 100)
 		height.append(rect[3] + 100)
 		for n in range(len(x)):
 			dst = frame[y[n]:y[n] + 10 + 3 * height[n], x[n]:x[n] + 30 + width[n]]
-			newImage_path = image_path + "/" + now + "_" + str(i) + '.jpg'
-			cv2.imwrite(newImage_path, dst)
+			imgResize = cv2.resize(dst, (resize, resize))
+			newImage_path = image_path + "/" + str(i) + '.jpg'
+			cv2.imwrite(newImage_path, imgResize)
 			i += 1
 
 		time.sleep(3)
@@ -62,26 +55,18 @@ def faceDetect(frame, cameraNum, save_file):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='face detect')
 	parser.add_argument('--data', '-d', type=str, default='others')
+	parser.add_argument('--size', '-s', type=int, default=128)
 	args = parser.parse_args()
 
 	cap = cv2.VideoCapture(0)
-	print cap
-
 	while True:
 		ret, frame = cap.read()
-		cv2.imshow('camera0 capture', frame)
-        # ret, frame1 = cap[1].read()
+		cv2.imshow('camera capture', frame)
+		th0 = faceDetect(frame, cameraNum[0], args.data, args.size)
 
-		th0 = faceDetect(frame, cameraNum[0], 'me')
-		#print th0
-        # th1 = FaceThread(frameresize1, cameraNum[1])
-
-		#th0.start()
-        # th1.start()
-
-        # cv2.imshow('camera1 capture', frameresize1)
+		k = cv2.waitKey(10)
+		if k == 27:
+			break
 
 	cap.release()
-    # cap[1].release()
-
 	cv2.destroyAllWindows()
