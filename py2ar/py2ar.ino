4d02d7760ec6d74e10189d8a6285bea1cf43273a
K-rot-0615 <dk.mat.18.of.a.challenge@gmail.com>
2016-10-29 03:36:47 +0900

    connect2arduino
    connect from python to arduino, using analyzed data with chainer

---
+++ py2ar/py2ar.ino
@@ -0,0 +1,20 @@
+void setup() {
+  pinMode(13,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int inputchar = Serial.read();
+
+  if(inputchar != -1){
+    switch(inputchar){
+      case 's':
+        digitalWrite(13,HIGH);
+        break;
+      case 'q':
+        digitalWrite(13,LOW);
+        break;
+    }
+  }
+}
