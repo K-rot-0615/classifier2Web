d352d052aff532816ae8c1c26899b38afa99ab2f
K-rot-0615 <dk.mat.18.of.a.challenge@gmail.com>
2016-11-07 23:47:18 +0900

    always predict with web camera

--- predict.py
+++ predict.py
@@ -1,27 +1,59 @@
 import argparse
+from image import faceDetect
 from image import faceRead
 from labeling import getPredictData
+from alexnet import Alex
 
+import cv2
 import chainer
 import chainer.functions as F
+import chainer.links as L
+from chainer import Variable
 
 
 def predict_image(model, data):
-    x = Valiable(data)
+    x = Variable(data)
     y = F.softmax(model.predictor(x.data[0]))
-    return y
+    return y.data[0]
 
 
 def main():
     parser = argparse.ArgumentParser(description='face prediction')
-    parser.add_argument('--data', '-d', type=str, default='./datasets/ryota' )
-    parser.add_argument('--model', '-m', type=str)
+    parser.add_argument('--data', '-d', type=str, default='./datasets/test2/')
+    parser.add_argument('--size', '-s', type=int, default=128)
+    parser.add_argument('--model', '-m', type=str, default='output2.model')
+    parser.add_argument('--channel', '-c', type=int, default=3)
 
-    images = faceRead(args.data)
-    for image in images:
-        test_data = getPredictData(image)
-        result = predict_image(args.model, args.data)
-        print result
+    args = parser.parse_args()
+
+    #load model and translate it from gpu model to cpu model
+    model = L.Classifier(Alex())
+    chainer.serializers.load_npz(args.model, model)
+    model.to_cpu()
+
+    #save file as predicted data
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret,frame = cap.read()
+        #cv2.imshow('predicted data', frame)
+        predicted_data = faceDetect(frame, 0, args.data, args.size)
+
+        # predict
+        images = faceRead(args.data)
+        #print images
+        for image in images:
+            #print image
+            test_data = getPredictData(image, args.channel)
+            result = predict_image(model, test_data)
+            print result
+
+        # escape from the roop
+        k = cv2.waitKey(10)
+        if k == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
